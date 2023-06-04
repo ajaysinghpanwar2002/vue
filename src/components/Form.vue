675d0810922ca805f43e8ffd455a99e8e5ef7e2b
ajaysinghpanwar2002 <ajaysingh07032003@gmail.com>
2023-06-04 16:18:19 +0530

    done

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,24 +1,31 @@
 <template>
     <h1>Get input field</h1>
-    <input type="text" placeholder="enter email" v-model="email"/>
-    <br/>
-    <input type="password" placeholder="enter password" v-model="password"/>
-    <br/>
+    <input type="text" placeholder="enter email" v-model="email" />
+    <br />
+    <input type="password" placeholder="enter password" v-model="password" />
+    <br />
     <button @click="getData()" type="button">Get values</button>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: "Form",
-    data(){
-        return{
-            email: null,
-            password: null,
+    data() {
+        return {
+            email: '',
+            password: '',
         }
     },
-    methods:{
-        getData(){
-            console.log("email: ",this.email,"password: ", this.password)
+    methods: {
+        async addUser() {
+            console.warn(this.email, this.password);
+            let result = await axios.post(url, {
+                email: this.email,
+                password: this.password
+            });
+            console.warn("result", result)
         }
     }
 }
